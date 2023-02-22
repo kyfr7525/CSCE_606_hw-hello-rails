f9b60de786dcf5a2e41df72c4fc78cace2ea6440
EC2 Default User <ec2-user@ip-172-31-5-192.ec2.internal>
2023-02-22 21:28:40 +0000

    more movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Aristocats', :rating => 'G',
+    :release_date => '24-Dec-1970'},
+  {:title => 'White Snake', :rating => 'PG-13',
+    :release_date => '11-Jan-2019'}
 ]
 
 more_movies.each do |movie|
